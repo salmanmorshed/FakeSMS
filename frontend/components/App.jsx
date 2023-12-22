595e0593fc37bf1df146e6d436aaa730538c00e6
Md. Salman Morshed <salmanmorshed@gmail.com>
2023-12-22 21:04:40 +0600

    switch to HashRouter

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home.jsx";
 import Previews from "./Previews.jsx";
 import Thread from "./Thread.jsx";
@@ -9,13 +9,13 @@ export default function App() {
         <>
             <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-200 text-gray-800 p-10">
                 <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
-                    <BrowserRouter>
+                    <HashRouter>
                         <Routes>
                             <Route path="/" element={<Home />}></Route>
                             <Route path="/:identity" element={<Previews />}></Route>
                             <Route path="/:identity/:target" element={<Thread />}></Route>
                         </Routes>
-                    </BrowserRouter>
+                    </HashRouter>
                 </div>
             </div>
         </>
